97638e459a88705f2c32b13ebd746ba8c7b648bf
Zuhaib Akram <info@zuhaib.pro>
2026-07-22 21:52:06 +0500

    feat: harden local MVP and add offline shell

---
+++ app/public/sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'metabotism-shell-v1'
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/','/manifest.webmanifest']))))
+self.addEventListener('fetch', event => { if (event.request.method === 'GET') event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response }).catch(() => caches.match('/')))) })
